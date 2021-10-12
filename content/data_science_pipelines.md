f2732fc1558ba92cc3c9b655f9e460268ebea8ce
nathanbegbie <nathanbegbie@gmail.com>
2021-10-12 21:13:27 +0300

    intial CI deploy

---
+++ content/data_science_pipelines.md
@@ -0,0 +1,11 @@
+Title: Why You Need Pipelines with SKLearn
+Date: 2021-10-12 20:30
+Category: Data Science
+
+## Introduction
+
+I am a big fan of data science
+
+## Main Body
+
+This is some serious quality content
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = "Nathan Begbie"
+SITENAME = "Artificially Intelligent"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/London"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = "./custom_theme"
+BIO = "Data Science for those who are not sure they know, y'know?"
+PROFILE_IMAGE = "profile.png"
